9d437c2a04ee8bfe2dd547b1b459d12e51d8ec30
Phillip Harris <phillip.harris@atlanticbt.com>
2015-05-11 11:22:30 -0400

    Initial Commit

---
+++ UnlockEverythingKitExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UnlockEverythingKitExample
+//
+//  Created by Phillip Harris on 5/11/15.
+//  Copyright (c) 2015 Phillip Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UnlockEverythingKitExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UnlockEverythingKitExample
+//
+//  Created by Phillip Harris on 5/11/15.
+//  Copyright (c) 2015 Phillip Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
